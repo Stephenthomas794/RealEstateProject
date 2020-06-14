d24575cbc9db84b3a5083096641716fc31bd3126
Stephenthomas794 <stephenthomas794@gmail.com>
2020-06-14 10:02:02 -0400

    Comments

--- RealEstateProject/app.py
+++ RealEstateProject/app.py
@@ -1,14 +1,23 @@
-# Libaries 
+# Libaries
+
+# Flask 
+#  - Flask is a lightweight WSGI web framework
+#  - Helps gets started quickly with building and scaling an application
 from flask import Flask
 
 # - To make HTTP Requests
+# - Automatically adds query strings, by adding parameters. But I did not do that
+# - Keep Alive and https connection pooling is done automatically 
 import requests 
 
 # - To prevent IP address blacklist from too many Requests
 import time
 from random import randint
 
-# - Data Science Libaries to parse ZipCode Files
+# Data Science Libaries to parse ZipCode Files
+# Numpy is not used in this application 
+# Numpys is used for multidiemntional arrays 
+# Pandas is great for in-memory 2d table objects called Dataframe
 import numpy as np
 import pandas as pd
 
@@ -27,16 +36,27 @@ app = Flask(__name__)
 wsgi_app = app.wsgi_app
 
 # Routing to Main Page
+# This allows to route to different webpages
+# There is only one webpage for this application
 @app.route('/')
+
+# Calls the "CheckZipCode" function
+# Converts it to a string
+# Returns to main page 
 def hello():
     value = str(CheckZipCodes())
     return value
 
 # For every 1000 zipCode Searches Create a new File
+# Have this function here as backup to generate a file
+# This was to store information locally to prevent a rerun of the entire application
 def GenerateFile(timeToCreate):
     pass
 
 # Email File every 1000 zipCodes
+# Unused code 
+# Copy and Paste from internet
+# Default
 def EmailFile():
     port = 465  # For SSL
     password = input()
@@ -48,7 +68,17 @@ def EmailFile():
         server.login("my@gmail.com", password)
         # TODO: Send email here
 
+# Checking all the zip codes
 def CheckZipCodes():
+
+    # Opens totals.txt file for storing data locally
+    # The file reader has been given the aplication to create a new file is totals does not exist and write to file
+    # Calls the cleanAndReturnZip function to get all the zipcodes
+    # Sets the word we are looking for in MLS as "listingID:"
+    # This means we know for everytime we find a listingID on that page, there is a foreclosure
+    # Totals variable is used to keep track of total amount of forclosures
+    # Counter variable is used to keep track of what zipCode we are on in the zipCode List
+    # CountOfForeclosures keeps track of how many foreclosures at that specific zipCOde
     f= open("Totals0.txt","w+")
     zipCodes = list()
     zipCodes = cleanAndReturnZip()
@@ -58,6 +88,11 @@ def CheckZipCodes():
     countOfForclosures = 0
 
     # Make HHTP request for every zip code to the URL
+    # For each zipCode in the zipCode list 
+    # First write the URl with its query string (so its MLS website + zipcode number"
+    # Make the request using requests libaray
+    # Turn the response into text
+    # Split the request into a new line
     for item in zipCodes:
         url = "https://mls.foreclosure.com/listing/search?lc=foreclosure&loc=" + item
         requestUrl = requests.get(url)
@@ -65,20 +100,35 @@ def CheckZipCodes():
         requestTextSplit = requestText.split('\n')
 
         # Check every line to see if it equals "var marketdata"
+        # For each line of the esponse we are looking for 'var markerdata" which is where listing IDs are stored
         for lines in requestTextSplit:
             if lines[0:15].lower() == 'var markersdata':
 
                 #F Count the number of times "listingID:" shows up to see the number of postings for that zip code
+                # We use the regex libaray (re), and sum function to find 
+                # finditer from re libaray allows up to find all non overlapping matches in a string 
+                # escape from re libary ignores all special characters
+                # Once we find total number of times "listing ID:" is found, we set that equal o countOFForclosures
+                # We then add this count to the total varible 
+                # print to command line the count of foreclosures for this particular zipcode
+                # write to file the count of forclosures for this particluar zip code
                 countOfForclosures = sum(1 for _ in re.finditer(r'\b%s\b' % re.escape(word), lines))   
                 total = total + countOfForclosures
-                print("Zip Code: {} Total Forclosures: {}".format(zipCodes[counter],countOfForclosures))
+                print("Zip Code: {} Total Forclosures: {} \n".format(zipCodes[counter],countOfForclosures))
                 f.write("Zip Code: {} Total Forclosures: {} \n".format(zipCodes[counter],countOfForclosures))
 
-        # Reset values and start the sleep timer
+        # Reset count Of Foreclosures 
+        # add one to counter to move to next zipcode
+        # Set sleep timer to avoid the website knowing we are a bot
+        # Sleep timer waits between 0 and 1 second
         countOfForclosures = 0
         counter = counter + 1
         time.sleep(randint(0,150)/100)
     
+    # print to command line the total number of foreclosures
+    # write to file total number of forclosures
+    # close the file 
+    # return total number of foreclosures
     print("Total Overall Forclosures: {}".format(total))
     f.write("Total Overall Forclosures: {}".format(total))
     f.close() 
@@ -86,6 +136,16 @@ def CheckZipCodes():
 
 # Clean the csv File and return zip codes as strings
 # Columns Avaliable: Zip, City, State ID, Population, County Names
+# Clean up the file by dropping all un needed columns 
+# First read the CSV file and turn into a data frame using pandas (zip varibale)
+# Dataframes allow you to handle the information easier
+# Nxt Drop all unneed columns 
+# Used the pandas libaray and the drop property
+# Used the head property to make sure I only have what I needed
+# Created a new varibale zipCodes and turned it into a list
+# Used the map function to iterate over only the zip codes in the data frame
+# And turn all the values into strings 
+# Returned the new list of all ZipCOdes as a list 
 def cleanAndReturnZip():
     zip = pd.read_csv('templates/uszips.csv')
     zip.drop('timezone',axis=1,inplace=True)
